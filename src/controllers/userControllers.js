dd38ea4ae15fbfd2a6727ba4c070281f025d1039
tomasAybar <aybar.t4@gmail.com>
2023-02-06 15:18:30 -0300

    controlador signup, registro de nuevo usuario

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,4 +1,5 @@
 const UserModel = require('../models/userModel');
+const bcryptjs = require('bcryptjs');
 
 const userControllers = {
 
@@ -23,44 +24,61 @@ const userControllers = {
         })
     },
 
-    addUser: async (req, res) => {
-
-        console.log('Controlador addUsers');
-
-        const { userName, firstName, lastName, email, password, photoUrl } = req.body
-
-        console.log(req.body);
-
-        let newUser;
-        let error = null;
-
-        try {
-
-            newUser = await new UserModel({
-                userName,
-                firstName,
-                lastName,
-                email,
-                password,
-                photoUrl,
-            }).save()
-
-        } catch (err) { error = err }
-
-        res.json({
-            response: error ? 'ERROR' : newUser,
-            success: error ? false : true,
-            error: error,
-            message: 'Usuario agregado',
-        })
-    },
-
     signinUser: async (req, res) => {
         res.send('signin')
     },
 
     signupUser: async (req, res) => {
-        res.send('signup')
+
+        let { userName, firstName, lastName, email, password, photoUrl } = req.body;
+
+        try {
+
+            const userExistEmail = await UserModel.findOne({ email }); // busco un usuario que coincida con el email del registro
+            const userExistUserName = await UserModel.findOne({ userName }); // busco un usuario que coincida con el userName del registro
+
+
+            if (!userExistEmail && !userExistUserName) { // no existe. entonces creo uno nuevo
+
+                const hashWord = bcryptjs.hashSync(password, 10) //encripto la contraseña
+
+                const newUser = await new UserModel({
+                    userName,
+                    firstName,
+                    lastName,
+                    email,
+                    password: hashWord,
+                    photoUrl
+                })
+
+                await newUser.save();
+
+                return res.status(201).json({
+                    success: true,
+                    message: `Te has registrado correctamente`,
+                    user: newUser
+                });
+
+            } else { // ya existe
+
+                return res.status(400).json({
+                    success: true,
+                    message: `Hola ${firstName.toUpperCase()} ya te encuentras registrado con ese email o usuario`,
+                });
+
+            }
+
+
+        } catch (error) {
+
+            return res.status(400).json({
+                success: false,
+                message: error,
+                console: console.log(error)
+            });
+
+        }
+
     }
 
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,10 @@
 
 const Router = require('express').Router(); // uso el creador de rutas de express
 
-const { getUsers, addUser, signinUser, signupUser } = require('../controllers/userControllers'); // traigo a mis controladores
+const { getUsers, signinUser, signupUser } = require('../controllers/userControllers'); // traigo a mis controladores
 
 // asocio mis controladores con las rutas usando el metodo enrutador de express
-Router.route('/users')
-    .get(getUsers)
-    .post(addUser)
-
+Router.get('/users', getUsers)
 Router.post('/signin', signinUser)
 Router.post('/signup', signupUser)
 
