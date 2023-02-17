566b59e1091e4389ac0ace1fc34e7573474f6d25
tomasAybar <aybar.t4@gmail.com>
2023-02-17 13:07:54 -0300

    login

--- frontend/src/screens/login/LoginScreen.jsx
+++ frontend/src/screens/login/LoginScreen.jsx
@@ -9,7 +9,7 @@ export const LoginScreen = () => {
 
 	return (
 		<View style={style.alinearCentro}>
-			<Text style={style.title}>LoginScreen</Text>
+			<Text style={style.title}>LoginScreen 2</Text>
 
 			<TouchableOpacity>
 				<Button
