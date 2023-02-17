0f0703eea3bcb3747dacd8d2f890c3cf5ebd103b
Gaston Avogadro <gaston.avogadro@gmail.com>
2023-02-17 14:50:54 -0300

    rama rev creada

--- frontend/src/navigate/MainNavigation.js
+++ frontend/src/navigate/MainNavigation.js
@@ -1,22 +1,18 @@
-import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { NavigationContainer } from '@react-navigation/native'
-import { OnboardingStack } from './stacks/OnboardingStack'
-import { LoginStack } from './stacks/LoginStack'
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { OnboardingStack } from './stacks/OnboardingStack';
+import { LoginStack } from './stacks/LoginStack';
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 export const MainNavigation = () => {
     return (
         <NavigationContainer>
-
-            <Stack.Navigator
-                screenOptions={{ headerShown: false }}
-                initialRouteName='OnboardingStack'
-            >
-                <Stack.Screen name='OnboardingStack' component={OnboardingStack} />
-                <Stack.Screen name='LoginStack' component={LoginStack} />
+            <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="OnboardingStack">
+                <Stack.Screen name="OnboardingStack" component={OnboardingStack} />
+                <Stack.Screen name="LoginStack" component={LoginStack} />
             </Stack.Navigator>
         </NavigationContainer>
-    )
-}
+    );
+};
