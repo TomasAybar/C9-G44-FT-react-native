0962ea3e7b613439277dd92ba60a686c980dcb6b
tomasAybar <aybar.t4@gmail.com>
2023-02-22 10:25:31 -0300

    componente input y componente btn navigate

---
+++ frontend/src/components/EcoBtnNavigate.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { MaterialIcons } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
+import { Text, TouchableOpacity, StyleSheet } from 'react-native'
+import theme from '../themes/theme'
+
+export const EcoBtnNavigate = ({ text = 'texto', navigate }) => {
+	const navigation = useNavigation()
+
+	return (
+		<TouchableOpacity
+			style={style.toucheable}
+			onPress={() =>
+				navigate
+					? navigation.navigate(navigate)
+					: alert('falta la navegacion')
+			}
+		>
+			<Text style={style.text}>{text}</Text>
+
+			<MaterialIcons
+				name='arrow-forward-ios'
+				size={24}
+				color={theme.colors.yellowPrimary}
+				style={{ textAlign: 'center' }}
+			/>
+		</TouchableOpacity>
+	)
+}
+
+const style = StyleSheet.create({
+	toucheable: {
+		backgroundColor: theme.colors.greyPrimary,
+		paddingHorizontal: 12,
+		borderRadius: 10,
+		height: 60,
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		flexDirection: 'row',
+	},
+	text: {
+		color: '#fff',
+		textAlign: 'center',
+	},
+})
---
+++ frontend/src/components/EcoInput.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import { StyleSheet, TextInput } from 'react-native'
+import theme from '../themes/theme'
+
+export const EcoInput = ({ type = 'text', placeholder = '' }) => {
+	const [onchangeInput, setOnchangeInput] = useState('')
+
+	return (
+		<TextInput
+			style={style.input}
+			value={onchangeInput}
+			keyboardType={type}
+			placeholder={placeholder}
+			placeholderTextColor={'#fff'}
+			onChangeText={setOnchangeInput}
+		/>
+	)
+}
+
+const style = StyleSheet.create({
+	input: {
+		height: 60,
+		paddingHorizontal: 12,
+		backgroundColor: theme.colors.greyPrimary,
+		borderRadius: 10,
+		color: '#fff',
+	},
+})
