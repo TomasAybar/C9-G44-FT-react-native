bf83be413b7a9cb53f184eed17865abb18fae18f
tomasAybar <aybar.t4@gmail.com>
2023-02-23 17:56:40 -0300

    fixed/merge

--- frontend/src/screens/login/RegistroExitoso.jsx
+++ frontend/src/screens/login/RegistroExitoso.jsx
@@ -1,112 +1,112 @@
-import React, {useState} from 'react';
-import { useNavigation } from '@react-navigation/native';
-import StyledText from '../../styledComponents/StyledText';
-import StyledView from '../../styledComponents/StyledView';
-import StyledButtton from '../../styledComponents/StyledButton';
-import {Alert, Modal, StyleSheet} from 'react-native';
-import theme from '../../themes/theme';
+import React, { useState } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import StyledText from '../../styledComponents/StyledText'
+import StyledView from '../../styledComponents/StyledView'
+import StyledButtton from '../../styledComponents/StyledButton'
+import { Alert, Modal, StyleSheet } from 'react-native'
+import theme from '../../themes/theme'
 
 export const RegistroExitoso = () => {
-    const [modalVisible, setModalVisible] = useState(false);
-  const navigator = useNavigation();
+	const [modalVisible, setModalVisible] = useState(false)
+	const navigator = useNavigation()
 
-  const handleCloseModal = () => {
-    setModalVisible(!modalVisible);
-    navigator.navigate('CompletarPerfilScreen');
-  };
+	const handleCloseModal = () => {
+		setModalVisible(!modalVisible)
+		navigator.navigate('CompletarPerfilScreen')
+	}
 
-  const handleCloseModal2 = () => {
-    setModalVisible(!modalVisible);
-    navigator.navigate('LoginScreen');
-  };
+	const handleCloseModal2 = () => {
+		setModalVisible(!modalVisible)
+		navigator.navigate('LoginScreen')
+	}
 
-    return (
-        <StyledView dark>
-            <StyledView container center>
-            <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert('Modal has been closed.');
-          setModalVisible(!modalVisible);
-        }}>
-        <StyledView container center >
-          <StyledView style={styles.modalView}>
-            <StyledText size16 weight700 marginBottom>Atención!</StyledText>
-            <StyledText size12 marginBottom >
-                Recordá que necesitarás que tu perfil este completo en caso que quieras vender o comprar.
-                </StyledText>
+	return (
+		<StyledView dark>
+			<StyledView container center height100>
+				<Modal
+					animationType='slide'
+					transparent={true}
+					visible={modalVisible}
+					onRequestClose={() => {
+						Alert.alert('Modal has been closed.')
+						setModalVisible(!modalVisible)
+					}}
+				>
+					<StyledView container center height100>
+						<StyledView style={styles.modalView}>
+							<StyledText size16 weight700 marginBottom>
+								Atención!
+							</StyledText>
+							<StyledText size12 marginBottom>
+								Recordá que necesitarás que tu perfil este
+								completo en caso que quieras vender o comprar.
+							</StyledText>
 
-                <StyledText>
-                    </StyledText>
+							<StyledText></StyledText>
 
-                <StyledButtton
-                    white
-                    title={'Completar Perfil'}
-                    onPress={handleCloseModal}
-                ></StyledButtton>
-                <StyledText>
-                    </StyledText>
+							<StyledButtton
+								white
+								title={'Completar Perfil'}
+								onPress={handleCloseModal}
+							></StyledButtton>
+							<StyledText></StyledText>
 
-      <StyledButtton
-                    black
-                    title={'Omitir'}
-                    onPress={handleCloseModal2}
-                ></StyledButtton>
-          </StyledView>
-        </StyledView>
-      </Modal>
-                <StyledText size16 weight700 marginBottom >
-                    ¡Felicitaciones!
-                </StyledText>
-                <StyledText size16 weight700 marginBottom marginTop>
-                    Tu cuenta fue creada con éxito.
-                </StyledText>
-                <StyledText size12 marginBottom marginTop>
-                    Ahora te vamos a pedir unos datos para completar tu perfil.
-                </StyledText>
-                <StyledText>
-                    </StyledText>
-                <StyledButtton
-                    white
-                    title={'Comenzar'}
-                    onPress={() => navigator.navigate('CompletarPerfilScreen')}
-                ></StyledButtton>
-                <StyledText>
-                    </StyledText>
-                <StyledButtton
-                    black
-                    title={'Omitir'}
-                    onPress={() => setModalVisible(true)}
-                ></StyledButtton>
-            </StyledView>
-        </StyledView>
-    );
-};
+							<StyledButtton
+								black
+								title={'Omitir'}
+								onPress={handleCloseModal2}
+							></StyledButtton>
+						</StyledView>
+					</StyledView>
+				</Modal>
+				<StyledText size16 weight700 marginBottom>
+					¡Felicitaciones!
+				</StyledText>
+				<StyledText size16 weight700 marginBottom marginTop>
+					Tu cuenta fue creada con éxito.
+				</StyledText>
+				<StyledText size12 marginBottom marginTop>
+					Ahora te vamos a pedir unos datos para completar tu perfil.
+				</StyledText>
+				<StyledText></StyledText>
+				<StyledButtton
+					white
+					title={'Comenzar'}
+					onPress={() => navigator.navigate('CompletarPerfilScreen')}
+				></StyledButtton>
+				<StyledText></StyledText>
+				<StyledButtton
+					black
+					title={'Omitir'}
+					onPress={() => setModalVisible(true)}
+				></StyledButtton>
+			</StyledView>
+		</StyledView>
+	)
+}
 const styles = StyleSheet.create({
-    centeredView: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginTop: 22,
-    },
-    modalView: {
-      margin: 20,
-      width: '90%',
-      height: '50%' ,
-      backgroundColor: theme.colors.greyPrimary,
-      borderRadius: 16,
-      padding: 35,
-      justifyContent: 'center',
-      alignItems: 'center',
-      shadowColor: '#000',
-      shadowOffset: {
-        width: 0,
-        height: 2,
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 4,
-      elevation: 5,
-    }
-  })
+	centeredView: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginTop: 22,
+	},
+	modalView: {
+		margin: 20,
+		width: '90%',
+		height: '50%',
+		backgroundColor: theme.colors.greyPrimary,
+		borderRadius: 16,
+		padding: 35,
+		justifyContent: 'center',
+		alignItems: 'center',
+		shadowColor: '#000',
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		},
+		shadowOpacity: 0.25,
+		shadowRadius: 4,
+		elevation: 5,
+	},
+})
--- frontend/src/screens/login/RegistroNombreScreen.jsx
+++ frontend/src/screens/login/RegistroNombreScreen.jsx
@@ -1,113 +1,109 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import StyledView from '../../styledComponents/StyledView'
 import StyledButton from '../../styledComponents/StyledButton'
 import StyledText from '../../styledComponents/StyledText'
 import StyledTextInput from '../../styledComponents/StyledTextInput'
-import { Formik, useField }  from 'formik'
-import { StyleSheet, Text  } from 'react-native'
+import { Formik, useField } from 'formik'
+import { StyleSheet, Text } from 'react-native'
 import { loginValidationShema } from '../../validationSchema/validationsRegister'
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native'
 
 const initialValues = {
-    name: '',
-    email: '',
-    password: '',
-    repassword: ''
+	name: '',
+	email: '',
+	password: '',
+	repassword: '',
 }
 
 const styles = StyleSheet.create({
-    error:{
-        color: 'red',
-        fontStyle: 'italic',
+	error: {
+		color: 'red',
+		fontStyle: 'italic',
 		opacity: 0.6,
-        fontSize: 12.8,
-        alignSelf: 'stretch',
-        marginBottom: 5,
-    },
+		fontSize: 12.8,
+		alignSelf: 'stretch',
+		marginBottom: 5,
+	},
 })
 
-const FormikInputValue = ({name, ...props}) => {
-    const [field, meta, helpers] = useField(name)
-    return(
-        <>
-        <StyledTextInput
-            error={meta.error}
-            value={field.value}
-            onChangeText={value => helpers.setValue(value)}
-            {... props}
-        />
-        {meta.error && <Text style={styles.error}>{meta.error}</Text>}
-        </>
-  )
+const FormikInputValue = ({ name, ...props }) => {
+	const [field, meta, helpers] = useField(name)
+	return (
+		<>
+			<StyledTextInput
+				error={meta.error}
+				value={field.value}
+				onChangeText={(value) => helpers.setValue(value)}
+				{...props}
+			/>
+			{meta.error && <Text style={styles.error}>{meta.error}</Text>}
+		</>
+	)
 }
 
 export const RegistroNombreScreen = () => {
-    const navigator = useNavigation();
+	const navigator = useNavigation()
 
-  return (
-    
-    <Formik validationSchema={loginValidationShema} initialValues={initialValues} 
-    onSubmit={values => console.log(values)}>
-        {({ handleChange, handleSubmit, values }) => {
-            return (
-                <StyledView dark>
-					<StyledView container center>
+	return (
+		<Formik
+			validationSchema={loginValidationShema}
+			initialValues={initialValues}
+			onSubmit={(values) => console.log(values)}
+		>
+			{({ handleChange, handleSubmit, values }) => {
+				return (
+					<StyledView dark>
+						<StyledView container center height100>
+							<StyledText size12 alignItem marginTop>
+								Nombre y Apellido
+							</StyledText>
+							<FormikInputValue
+								name='name'
+								placeholder='Nombre y Apellido'
+								placeholderTextColor='rgba(255,255,255,0.5)'
+							/>
 
-					<StyledText size12 alignItem marginTop>
-					Nombre y Apellido
-					</StyledText> 
-					<FormikInputValue 
-                    name = 'name'
-                    placeholder='Nombre y Apellido'
-                    placeholderTextColor='rgba(255,255,255,0.5)'
-                    />
+							<StyledText size12 alignItem alignSelf marginTop>
+								E-mail
+							</StyledText>
+							<FormikInputValue
+								name='email'
+								placeholder='E-mail'
+								placeholderTextColor='rgba(255,255,255,0.5)'
+							/>
 
-					<StyledText size12 alignItem alignSelf marginTop>
-						E-mail
-					</StyledText>
-                    <FormikInputValue 
-                    name = 'email'
-                    placeholder='E-mail'
-                    placeholderTextColor='rgba(255,255,255,0.5)'
-                    />
+							<StyledText size12 alignItem alignSelf marginTop>
+								Contraseña
+							</StyledText>
+							<FormikInputValue
+								name='password'
+								placeholder='Contraseña'
+								placeholderTextColor='rgba(255,255,255,0.5)'
+								secureTextEntry
+							/>
 
-					<StyledText size12 alignItem alignSelf marginTop>
-					Contraseña
-					</StyledText>
-                    <FormikInputValue
-                    name = 'password'
-                    placeholder='Contraseña'
-                    placeholderTextColor='rgba(255,255,255,0.5)'
-                    secureTextEntry
-                    />
-					
-					<StyledText size12 alignItem alignSelf marginTop>
-					   Repetir Contraseña
-					</StyledText>
-					<FormikInputValue 
-                    name = 'repassword'
-                    placeholder='Repetir contraseña'
-                    placeholderTextColor='rgba(255,255,255,0.5)'
-					secureTextEntry
-                    />
-                    <StyledText marginTop>
-                        {'\n'}
-                    </StyledText>
+							<StyledText size12 alignItem alignSelf marginTop>
+								Repetir Contraseña
+							</StyledText>
+							<FormikInputValue
+								name='repassword'
+								placeholder='Repetir contraseña'
+								placeholderTextColor='rgba(255,255,255,0.5)'
+								secureTextEntry
+							/>
+							<StyledText marginTop>{'\n'}</StyledText>
 
-            
-                    <StyledButton
-                    white
-					title={'Crear cuenta'}
-					onPress={() => navigator.navigate('RegistroExitoso')}
-					></StyledButton>
-
-
+							<StyledButton
+								white
+								title={'Crear cuenta'}
+								onPress={() =>
+									navigator.navigate('RegistroExitoso')
+								}
+							></StyledButton>
+						</StyledView>
 					</StyledView>
-                </StyledView>
-            )
-        }}
-
-    </Formik>
-    )
-
+				)
+			}}
+		</Formik>
+	)
 }
