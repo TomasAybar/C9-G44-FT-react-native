1806bf09275cd1a33b3dd0eb3a9de68d0f0ef416
tomasAybar <aybar.t4@gmail.com>
2023-03-02 00:45:37 -0300

    statusbar

--- frontend/App.js
+++ frontend/App.js
@@ -2,10 +2,10 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { MainNavigation } from './src/navigate/MainNavigation';
 import { useFonts, Inter_400Regular, Inter_500Medium, Inter_700Bold, Inter_600SemiBold } from '@expo-google-fonts/inter';
-
-// import second from 'react-import { connect } from 'react-redux''
+import { StatusBar } from 'react-native';
 import { Provider } from 'react-redux'
 import store from './store';
+import theme from './src/themes/theme';
 
 
 export default function App() {
@@ -22,8 +22,13 @@ export default function App() {
 
     return (
         <Provider store={store}>
+            <StatusBar
+                backgroundColor={theme.colors.appBackground}
+                barStyle='light-content'
+            />
 
             <MainNavigation />
+
 
         </Provider>
     )
