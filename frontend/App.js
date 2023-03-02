0b29daa66f288b8bf9827055504c5fc1968e7eb0
tomasAybar <aybar.t4@gmail.com>
2023-03-02 16:13:06 -0300

    borrar

--- frontend/App.js
+++ frontend/App.js
@@ -7,6 +7,8 @@ import { Provider } from 'react-redux'
 import store from './store';
 import theme from './src/themes/theme';
 
+// borrame
+
 
 export default function App() {
     const [fontsLoaded] = useFonts({
